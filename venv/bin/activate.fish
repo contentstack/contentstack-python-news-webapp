1e3e5312578be1e87ad30cd9a21e26733cb307ad
mshaileshr@gmail.com <Yes@383840>
2019-11-13 16:49:54 +0530

    code updated

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/shaileshmishra/Desktop/contentstack-news/venv"
+set -gx VIRTUAL_ENV "/Users/shaileshmishra/Documents/contentstack/workspace/python/Example/contentstack-python-news-webapp/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
