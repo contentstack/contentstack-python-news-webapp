bd269643d24eb38b9b4ac1a242214cb55f9a886f
mshaileshr@gmail.com <Yes@383840>
2019-11-18 11:10:41 +0530

    initial commit

--- app.py
+++ app.py
@@ -1,20 +1,18 @@
 from flask import Flask, render_template, url_for
-from contentstack import Stack
+import contentstack
 
 app = Flask(__name__)
-
-
-def fetch_news():
-    stack = Stack(api_key='***REMOVED***', access_token='***REMOVED***', environment='production')
-    query = stack.content_type('news').query()
-    response = query.find()
-    return response
+    
 
 
 @app.route('/')
 @app.route('/home')
-def home():
-    headlines = fetch_news()
+def news():
+    
+    stack = contentstack.Stack(api_key='***REMOVED***', access_token='***REMOVED***', environment='production')
+    query = stack.content_type('news').query()
+    headlines = query.find()
+
     return render_template('home.html', news=headlines, title="home")
 
 
