9b53932933e8e03fbf8830ec5eb7af7d3188e442
mshaileshr@gmail.com <Yes@383840>
2020-06-17 13:07:25 +0530

    1.0.0

--- app.py
+++ app.py
@@ -2,17 +2,14 @@ from flask import Flask, render_template, url_for
 import contentstack
 
 app = Flask(__name__)
-    
 
 
 @app.route('/')
 @app.route('/news')
 def news():
-    
-    stack = contentstack.Stack(api_key='blt920bb7e90248f607', access_token='blt0c4300391e033d4a59eb2857', environment='production')
+    stack = contentstack.Stack('blt920bb7e90248f607', 'blt0c4300391e033d4a59eb2857', 'production')
     query = stack.content_type('news').query()
     headlines = query.find()
-
     return render_template('home.html', news=headlines, title="home")
 
 
