19a580824ae0f340a2000125090ec359cac29522
raj pandey <raj.pandey@contentstack.com>
2025-07-25 16:08:02 +0530

    Add SECURITY.md with security policy and contact information

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/news')
 def news():
-    stack = contentstack.Stack('***REMOVED***', '***REMOVED***', 'production')
+    stack = contentstack.Stack('blt920bb7e90248f607', 'blt0c4300391e033d4a59eb2857', 'production')
     query = stack.content_type('news').query()
     headlines = query.find()
     return render_template('home.html', news=headlines, title="home")
